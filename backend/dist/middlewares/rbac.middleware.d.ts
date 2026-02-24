c8b9134aca033ba84571cf98f61f2caf14ded260
Rajika keminda <rkeminda0@gmail.com>
2026-02-24 22:54:43 +0530

    fix:remove dist

--- backend/dist/middlewares/rbac.middleware.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { UserRole } from '../models/user.model';
-export declare const authorize: (...allowedRoles: UserRole[]) => (req: Request, _res: Response, next: NextFunction) => void;
-//# sourceMappingURL=rbac.middleware.d.ts.map
